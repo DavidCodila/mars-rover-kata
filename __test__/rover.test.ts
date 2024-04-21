672b11574b1a229aac016a206dbea7287a635589
david <Davids-MacBook-Pro.local>
2024-04-22 09:35:30 +1000

    Added tests for rover movement in back, left and right directions

--- __test__/rover.test.ts
+++ __test__/rover.test.ts
@@ -14,3 +14,27 @@ test("moves forward test", () => {
   r.move(command);
   expect(r.getPosition().y).toEqual(2);
 });
+
+test("moves backw test", () => {
+  const initalPoint: point = { x: 1, y: 1 };
+  const r: rover = new rover(initalPoint, "N");
+  const command = ["b"];
+  r.move(command);
+  expect(r.getPosition().y).toEqual(0);
+});
+
+test("moves left test", () => {
+  const initalPoint: point = { x: 1, y: 1 };
+  const r: rover = new rover(initalPoint, "N");
+  const command = ["l"];
+  r.move(command);
+  expect(r.getPosition().x).toEqual(0);
+});
+
+test("moves right test", () => {
+  const initalPoint: point = { x: 1, y: 1 };
+  const r: rover = new rover(initalPoint, "N");
+  const command = ["r"];
+  r.move(command);
+  expect(r.getPosition().x).toEqual(2);
+});
