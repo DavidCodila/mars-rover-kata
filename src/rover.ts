98b71b51e54ea670d5b3516fd370f757fe1fc352
david <Davids-MacBook-Pro.local>
2024-04-22 14:51:57 +1000

    Refactored move command in rover.ts to make it more readable

--- src/rover.ts
+++ src/rover.ts
@@ -22,38 +22,81 @@ export class Rover {
   getOrientation() {
     return this.orientation;
   }
-  move(commandToMove: string[]): boolean {
-    var passesCommandSyntax = true;
-    this.commands = commandToMove;
-    for (var commandIndex in this.commands) {
-      var command = this.commands[commandIndex];
-      if (command === "f") {
-        if (this.position.y !== this.map.getMapRows() - 1) this.position.y++;
-        else {
-          this.position.y = 0;
-        }
-      } else if (command === "b") {
-        if (this.position.y !== 0) this.position.y--;
-        else {
-          this.position.y = this.map.getMapColumns();
-        }
-      } else if (command === "r") {
-        if (this.position.x !== this.map.getMapRows() - 1) this.position.x++;
-        else {
-          this.position.x = 0;
-        }
-      } else if (command === "l") {
-        if (this.position.x !== 0) this.position.x--;
-        else this.position.x = this.map.getMapRows();
-      } else {
-        passesCommandSyntax = false;
-      }
-    }
-    return passesCommandSyntax;
-  }
   getCommands(): string[] {
     return this.commands;
   }
+  move(commandToMove: string[]) {
+    this.commands = commandToMove;
+    for (var commandIndex in this.commands) {
+      var command = this.commands[commandIndex];
+      this.moveRoverRelativeTo(command);
+    }
+  }
+  moveRoverRelativeTo(command: string) {
+    if (moveForward(command)) {
+      if (this.isNotAtTopOfMap()) this.position.y++;
+      else {
+        this.position.y = 0;
+      }
+    } else if (moveBack(command)) {
+      if (this.isNotAtBottomOfMap()) this.position.y--;
+      else {
+        this.moveRoverToTopOfMap();
+      }
+    } else if (moveRight(command)) {
+      if (this.isNotAtRightEndOfMap()) this.position.x++;
+      else {
+        this.position.x = 0;
+      }
+    } else if (moveLeft(command)) {
+      if (this.isNotAtLeftEndOfMap()) this.position.x--;
+      else this.moveRoverToRightEndOfMap();
+    } else {
+      console.log("Error in move command: " + command);
+    }
+  }
+  isNotAtTopOfMap(): boolean {
+    if (this.position.y !== this.map.getMapRows() - 1) return true;
+    return false;
+  }
+  isNotAtBottomOfMap(): boolean {
+    if (this.position.y !== 0) return true;
+    return false;
+  }
+  isNotAtRightEndOfMap(): boolean {
+    if (this.position.x !== this.map.getMapRows() - 1) return true;
+    return false;
+  }
+  isNotAtLeftEndOfMap(): boolean {
+    if (this.position.x !== 0) return true;
+    return false;
+  }
+  moveRoverToTopOfMap() {
+    this.position.y = this.map.getMapColumns();
+  }
+  moveRoverToRightEndOfMap() {
+    this.position.x = this.map.getMapRows();
+  }
+}
+
+function moveForward(command: string): boolean {
+  if (command == "f") return true;
+  return false;
+}
+
+function moveBack(command: string): boolean {
+  if (command == "b") return true;
+  return false;
+}
+
+function moveRight(command: string): boolean {
+  if (command == "r") return true;
+  return false;
+}
+
+function moveLeft(command: string): boolean {
+  if (command == "l") return true;
+  return false;
 }
 
 export type point = {
