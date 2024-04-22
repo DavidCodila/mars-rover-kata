c1c1aa8402bd0d97273308de59d1046f7583daed
david <Davids-MacBook-Pro.local>
2024-04-22 12:19:22 +1000

    Removed code duplication in index.test.ts

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -3,29 +3,26 @@ import { Controller } from "../src/Controller";
 import { Rover, type point } from "../src/Rover";
 import { MarsMap } from "../src/MarsMap";
 
+const initalPoint: point = { x: 1, y: 1 };
+const controller: Controller = new Controller();
+
 test("receives commands test", () => {
-  const initalPoint: point = { x: 1, y: 1 };
   const initalCommands = "f f b";
   const rover: Rover = new Rover(initalPoint, "N", new MarsMap("3 3"));
-  const controller: Controller = new Controller();
   rover.move(controller.sendCommands(initalCommands));
   expect(rover.getCommands()).toEqual(initalCommands.split(" "));
 });
 
 test("Rover wraps around left edge", () => {
-  const initalPoint: point = { x: 1, y: 1 };
   const initalCommands = "l l";
   const rover: Rover = new Rover(initalPoint, "N", new MarsMap("3 3"));
-  const controller: Controller = new Controller();
   rover.move(controller.sendCommands(initalCommands));
   expect(rover.getPosition().x).toEqual(3);
 });
 
 test("Rover wraps around right edge", () => {
-  const initalPoint: point = { x: 1, y: 1 };
   const initalCommands = "r r";
   const rover: Rover = new Rover(initalPoint, "N", new MarsMap("3 3"));
-  const controller: Controller = new Controller();
   rover.move(controller.sendCommands(initalCommands));
   expect(rover.getPosition().x).toEqual(0);
 });
