c0c36af5ce3adb89bdc2b1c5f82ff20f33baafc5
david <Davids-MacBook-Pro.local>
2024-04-22 14:23:31 +1000

    Added checkCommandSyntax function and other validation in Controller.ts

--- src/controller.ts
+++ src/controller.ts
@@ -1,5 +1,19 @@
 export class Controller {
   sendCommands(commands: string) {
-    return commands.split(" ");
+    const commandArray = commands.split(" ");
+    const error = checkCommandSyntax(commandArray);
+    if (error) return [];
+    else return commandArray;
   }
+}
+
+function checkCommandSyntax(commandArray: string[]): boolean {
+  commandArray.forEach((command) => {
+    if (!isaMoveCommand(command)) return true;
+  });
+  return false;
+}
+
+function isaMoveCommand(input: string): boolean {
+  return ["f", "b", "l", "r"].includes(input);
 }
