d3e10a4fbe28eb03f3f68c48b45fc3bf7ba44541
david <Davids-MacBook-Pro.local>
2024-04-24 10:51:13 +1000

    Refactered moveRover command in the Rover.ts file

--- src/rover.ts
+++ src/rover.ts
@@ -4,6 +4,7 @@ export class Rover {
   private position: point = { x: 1, y: 1 };
   private orientation: string = "N";
   private commands: string[];
+  private currentComand: string;
   private map: MapTemplate;
   private objectDetected: boolean;
   constructor(
@@ -14,6 +15,7 @@ export class Rover {
     this.position = initalPosition;
     this.orientation = initalorientation;
     this.commands = [""];
+    this.currentComand = "";
     this.map = map;
     if (map.getMapColumns() === 4) map.createObstacles();
     this.objectDetected = false;
@@ -31,79 +33,112 @@ export class Rover {
   move(commandToMove: string[]): string {
     this.commands = commandToMove;
     for (var commandIndex in this.commands) {
-      if (this.objectDetected)
-        //flags before last command
-        return "Will hit object on move " + commandIndex;
+      if (this.objectDetected) return "Will hit object on move " + commandIndex;
       else {
-        var command = this.commands[commandIndex];
-        this.moveRoverRelativeTo(command);
+        this.setCurrentCommand(this.commands[commandIndex]);
+        this.moveRover();
       }
     }
-    if (this.objectDetected)
-      //flags last command
-      return "Will hit object on last command";
+    if (this.objectDetected) return "Will hit object on last command";
     else return "Move successful";
   }
-  moveRoverRelativeTo(command: string) {
-    if (moveForward(command)) {
-      if (this.isNotAtTopOfMap()) {
-        if (this.map.hasObstacle(this.position.x, this.position.y + 1))
-          this.objectDetected = true;
-        else this.position.y++;
-      } else {
-        if (this.map.hasObstacle(this.position.x, 0))
-          this.objectDetected = true;
-        else this.position.y = 0;
-      }
-    } else if (moveBack(command)) {
-      if (this.isNotAtBottomOfMap()) {
-        if (this.map.hasObstacle(this.position.x, this.position.y - 1))
-          this.objectDetected = true;
-        else this.position.y--;
-      } else {
-        var endOfMap = this.map.getMapColumns() - 1;
-        if (this.map.hasObstacle(this.position.x, endOfMap))
-          this.objectDetected = true;
-        else this.moveRoverToTopOfMap();
-      }
-    } else if (moveRight(command)) {
-      if (this.isNotAtRightEndOfMap()) {
-        if (this.map.hasObstacle(this.position.x + 1, this.position.y))
-          this.objectDetected = true;
-        else this.position.x++;
-      } else {
-        if (this.map.hasObstacle(0, this.position.y))
-          this.objectDetected = true;
-        else this.position.x = 0;
-      }
-    } else if (moveLeft(command)) {
-      if (this.isNotAtLeftEndOfMap()) {
-        if (this.map.hasObstacle(this.position.y, this.position.x - 1))
-          this.objectDetected = true;
-        else this.position.x--;
-      } else {
-        var endOfMap = this.map.getMapRows() - 1;
-        if (this.map.hasObstacle(endOfMap, this.position.y))
-          this.objectDetected = true;
-        else this.moveRoverToRightEndOfMap();
-      }
-    } else {
-      console.log("Error in move command: " + command);
+  setCurrentCommand(command: string) {
+    this.currentComand = command;
+    this.formatCommand();
+  }
+  formatCommand() {
+    switch (this.currentComand) {
+      case "f":
+        this.currentComand = "forward";
+        break;
+      case "b":
+        this.currentComand = "backward";
+        break;
+      case "r":
+        this.currentComand = "right";
+        break;
+      case "l":
+        this.currentComand = "left";
+        break;
+      default:
+        console.log("Error with command: " + this.currentComand);
     }
   }
-  isNotAtTopOfMap(): boolean {
+  moveRover() {
+    switch (this.currentComand) {
+      case "forward":
+        this.moveRoverForward();
+        break;
+      case "backward":
+        this.moveRoverBackward();
+        break;
+      case "right":
+        this.moveRoverRight();
+        break;
+      case "left":
+        this.moveRoverLeft();
+        break;
+      default:
+        console.log("Error in move command: " + this.currentComand);
+    }
+  }
+  moveRoverForward() {
+    if (this.roverIsNotAtTopOfMap()) {
+      if (this.map.hasObstacle(this.position.x, this.position.y + 1))
+        this.objectDetected = true;
+      else this.position.y++;
+    } else {
+      if (this.map.hasObstacle(this.position.x, 0)) this.objectDetected = true;
+      else this.position.y = 0;
+    }
+  }
+  moveRoverBackward() {
+    if (this.roverIsNotAtBottomOfMap()) {
+      if (this.map.hasObstacle(this.position.x, this.position.y - 1))
+        this.objectDetected = true;
+      else this.position.y--;
+    } else {
+      var endOfMap = this.map.getMapColumns() - 1;
+      if (this.map.hasObstacle(this.position.x, endOfMap))
+        this.objectDetected = true;
+      else this.moveRoverToTopOfMap();
+    }
+  }
+  moveRoverRight() {
+    if (this.roverIssNotAtRightEndOfMap()) {
+      if (this.map.hasObstacle(this.position.x + 1, this.position.y))
+        this.objectDetected = true;
+      else this.position.x++;
+    } else {
+      if (this.map.hasObstacle(0, this.position.y)) this.objectDetected = true;
+      else this.position.x = 0;
+    }
+  }
+  moveRoverLeft() {
+    if (this.roverIsNotAtLeftEndOfMap()) {
+      if (this.map.hasObstacle(this.position.y, this.position.x - 1))
+        this.objectDetected = true;
+      else this.position.x--;
+    } else {
+      var endOfMap = this.map.getMapRows() - 1;
+      if (this.map.hasObstacle(endOfMap, this.position.y))
+        this.objectDetected = true;
+      else this.moveRoverToRightEndOfMap();
+    }
+  }
+  roverIsNotAtTopOfMap(): boolean {
     if (this.position.y !== this.map.getMapRows() - 1) return true;
     return false;
   }
-  isNotAtBottomOfMap(): boolean {
+  roverIsNotAtBottomOfMap(): boolean {
     if (this.position.y !== 0) return true;
     return false;
   }
-  isNotAtRightEndOfMap(): boolean {
+  roverIssNotAtRightEndOfMap(): boolean {
     if (this.position.x !== this.map.getMapRows() - 1) return true;
     return false;
   }
-  isNotAtLeftEndOfMap(): boolean {
+  roverIsNotAtLeftEndOfMap(): boolean {
     if (this.position.x !== 0) return true;
     return false;
   }
@@ -113,26 +148,6 @@ export class Rover {
   moveRoverToRightEndOfMap() {
     this.position.x = this.map.getMapRows();
   }
-}
-
-function moveForward(command: string): boolean {
-  if (command == "f") return true;
-  return false;
-}
-
-function moveBack(command: string): boolean {
-  if (command == "b") return true;
-  return false;
-}
-
-function moveRight(command: string): boolean {
-  if (command == "r") return true;
-  return false;
-}
-
-function moveLeft(command: string): boolean {
-  if (command == "l") return true;
-  return false;
 }
 
 export type point = {
